dad29a21f0280143a8d4b357a65ba08a46f05b3f
mirelesadan <117174353+mirelesadan@users.noreply.github.com>
2022-12-09 17:14:12 -0600

    Update verifyCUDA.py

--- verifyCUDA.py
+++ verifyCUDA.py
@@ -1,17 +1,24 @@
-# Ensure that CUDA device is working if available.
+"""
+Verify that your system has a CUDA-enabled device.
+Print the following lines:
+"""
 
 import torch
 
 # CUDA (Compute Unified Device Architecture) is a parallel computing platform and programming model created by NVIDIA.
 
 torch.cuda.is_available()
+# Should print: 
 # >> True
 
 torch.cuda.device_count()
+# Should print, at least: 
 # >> 1
 
 torch.cuda.device(torch.cuda.current_device())
-# >> <torch.cuda.device at 0x193ddee57c0>
+# Should print something with the format:
+# >> <torch.cuda.device at 0x123abcd45d6>
 
 torch.cuda.get_device_name(torch.cuda.current_device())
-# >> 'NVIDIA GeForce MX150'
+# Should print something such as:
+# >> 'NVIDIA GeForce XX700'
